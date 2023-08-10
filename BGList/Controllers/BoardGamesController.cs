216f740d0dded2bac6df96645a55ea9bc59e0b20
Eduardo Correia <eduardo.vinicius2009@hotmail.com>
2023-08-10 19:14:21 -0300

    Add min and max players property to BoardGame DTO. Configure Kestrel launch profile for staging environment. Configure appsettings files for different environments. Configure app middleware pipeline based on configuration settings.

--- BGList/Controllers/BoardGamesController.cs
+++ BGList/Controllers/BoardGamesController.cs
@@ -22,19 +22,25 @@ namespace BGList.Controllers
                 {
                     Id = 1,
                     Name = "Axis & Allies",
-                    Year = 1981
+                    Year = 1981,
+                    MinPlayers = 2,
+                    MaxPlayers= 5,
                 },
                 new BoardGame
                 {
                     Id = 2,
                     Name = "Citadels",
-                    Year = 200
+                    Year = 200,
+                    MinPlayers = 2,
+                    MaxPlayers= 8,
                 },
                 new BoardGame
                 {
                     Id = 3,
                     Name = "Terraforming Mars",
-                    Year = 2016
+                    Year = 2016,
+                    MinPlayers = 1,
+                    MaxPlayers= 5,
                 }
             };
         }
