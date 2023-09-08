8b9651e774946dc8783cb03b68540b01276c24ff
Eduardo Correia <eduardo.vinicius2009@hotmail.com>
2023-09-08 10:56:06 -0300

    Add paging, sorting and filtering.

--- BGList/Controllers/BoardGamesController.cs
+++ BGList/Controllers/BoardGamesController.cs
@@ -2,6 +2,7 @@
 using BGList.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
 
 namespace BGList.Controllers
 {
@@ -23,17 +24,34 @@ namespace BGList.Controllers
 
         [HttpGet(Name = "GetBoardGames")]
         [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
-        public async Task<RestDTO<BoardGame[]>> Get()
+        public async Task<RestDTO<BoardGame[]>> Get(
+            int pageIndex = 0,
+            int pageSize = 10,
+            string? sortColumn = "Name",
+            string? sortOrder = "ASC",
+            string? filterQuery = null)
         {
-            var query = _context.BoardGames;
+            var query = _context.BoardGames.AsQueryable();
+            if (!string.IsNullOrEmpty(filterQuery))
+                query = query.Where(b => b.Name.Contains(filterQuery));
+
+            var recordCount = await query.CountAsync();
+
+            query = query
+                .OrderBy($"{sortColumn} {sortOrder}")
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize);
 
             return new RestDTO<BoardGame[]>()
             {
                 Data = await query.ToArrayAsync(),
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                RecordCount = recordCount,
                 Links = new List<LinkDTO>
                 {
                     new LinkDTO(
-                        Url.Action(null, "BoardGames", null, Request.Scheme)!,
+                        Url.Action(null, "BoardGames", new { pageIndex, pageSize }, Request.Scheme)!,
                         "self",
                         "GET")
                 }
